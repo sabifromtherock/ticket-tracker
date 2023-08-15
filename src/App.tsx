6b807f7b026e0a100b84512f96fdf9678ad1c18c
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-15 13:58:29 +0100

    added Select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.scss";
-import team from "./data/team";
 import EmployeesContainer from "./components/EmployeesContainer/EmployeesContainer";
 import Header from "./components/Header/Header";
 
@@ -7,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <EmployeesContainer team={team} />
+      <EmployeesContainer />
     </div>
   );
 }
