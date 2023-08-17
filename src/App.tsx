7576032d9c207844f06b81e3060190455063c469
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-17 12:49:56 +0100

    added /tickets endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import EmployeesContainer from "./components/EmployeesContainer/EmployeesContainer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <EmployeesContainer />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/tickets" element={<EmployeesContainer />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
