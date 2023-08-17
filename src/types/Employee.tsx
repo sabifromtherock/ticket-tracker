14ca568341adc4d21f5bf8a614a4321db4665920
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-08-17 12:41:11 +0100

    updated data and types

--- src/types/Employee.tsx
+++ src/types/Employee.tsx
@@ -1,6 +1,9 @@
+import { Profile } from "./Profile";
+
 export type Employee = {
   id: number;
   name: string;
   role: string;
   counter?: number;
+  profile: Profile;
 };
---
+++ src/types/Profile.tsx
@@ -0,0 +1,6 @@
+export type Profile = {
+  experience: string;
+  department: string;
+  techstack: string[];
+  profilePicture: string;
+};
